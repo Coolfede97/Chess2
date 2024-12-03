ebcd5ce68251dcc7789b8313ebe7bfe4a8ea66eb
Coolfede97 <coolfede97@gmail.com>
2024-12-03 17:35:05 -0300

    cambio a lista de candidatos

--- Chess-Challenge/src/My Bot/FedeBots/Test.cs
+++ Chess-Challenge/src/My Bot/FedeBots/Test.cs
@@ -12,18 +12,16 @@ public class Test
     public int extraDepth = 2;
     public Candidate Think(Board board, Timer timer)
     {
-        return MiniMax(board, depth, true, new Candidate(Move.NullMove,int.MinValue), int.MinValue, int .MaxValue);        
+        return MiniMax(board, depth, true, new Candidate(Move.NullMove,int.MinValue), int.MinValue, int .MaxValue)[0];        
     }
 
-    public Candidate MiniMax(Board board, int depth, bool isMaximizing, Candidate lastCandidate, int alpha, int beta)
+    public List<Candidate> MiniMax(Board board, int depth, bool isMaximizing, Candidate lastCandidate, int alpha, int beta)
     {
         if (depth == 0 || GameIsFinished(board))
         {
-            lastCandidate=QuiesceneSearch(board,extraDepth, isMaximizing,lastCandidate, alpha, beta);
-            return lastCandidate;
+            return QuiesceneSearch(board,extraDepth, isMaximizing,lastCandidate, alpha, beta);
         }
-        Candidate bestCandidate = new Candidate(Move.NullMove,isMaximizing ? int.MinValue : int.MaxValue);
-        int bestValue = isMaximizing ? int.MinValue : int.MaxValue;
+        List<Candidate> bestCandidates = new List<Candidate>();
         Move[] legalMoves = board.GetLegalMoves();
         legalMoves = OrderMoves(legalMoves,board);
         foreach (Move legalMove in legalMoves)
@@ -35,23 +33,29 @@ public class Test
             //     continue;
             // }
             Candidate newCandidate = new Candidate(legalMove,0);
-            Candidate candidate = MiniMax(board, depth-1, !isMaximizing, newCandidate, alpha, beta);
-            if (bestCandidate.movement == Move.NullMove) bestCandidate=new Candidate(legalMove,candidate.materialWon);
+            Candidate candidate = MiniMax(board, depth-1, !isMaximizing, newCandidate, alpha, beta)[0];
+            if (bestCandidates.Count==0) bestCandidates.Add(new Candidate(legalMove,candidate.materialWon));
             if (isMaximizing)
             {
-                if (candidate.materialWon>bestValue)
+                if (candidate.materialWon>bestCandidates[0].materialWon)
                 {
-                    bestValue=candidate.materialWon;
-                    bestCandidate = new Candidate(legalMove, candidate.materialWon);
+                    bestCandidates.Insert(0,new Candidate(legalMove, candidate.materialWon));
+                }
+                else
+                {
+                    bestCandidates.Add(new Candidate(legalMove, candidate.materialWon));
                 }
                 alpha = Math.Max(alpha, candidate.materialWon);
             }
             else
             {
-                if (candidate.materialWon<bestValue)
+                if (candidate.materialWon<bestCandidates[0].materialWon)
                 {
-                    bestValue=candidate.materialWon;
-                    bestCandidate = new Candidate(legalMove, candidate.materialWon);
+                    bestCandidates.Insert(0,new Candidate(legalMove, candidate.materialWon));
+                }
+                else
+                {
+                    bestCandidates.Add(new Candidate(legalMove, candidate.materialWon));
                 }
                 beta = Math.Min(beta,candidate.materialWon);
             }
@@ -61,37 +65,42 @@ public class Test
                 break;
             }
         }
-        return bestCandidate;
+        return bestCandidates;
     }
 
-    public Candidate QuiesceneSearch(Board board,int depth, bool isMaximizing, Candidate lastCandidate, int alpha, int beta)
+    public List<Candidate> QuiesceneSearch(Board board,int depth, bool isMaximizing, Candidate lastCandidate, int alpha, int beta)
     {
         List<Move> noisyMoves = GetNoisyMoves(board, isWhite);
         if (noisyMoves.Count>0 && depth>0)
         {
-            Candidate bestCandidate = new Candidate(Move.NullMove,isMaximizing ? int.MinValue : int.MaxValue);
-            int bestValue = isMaximizing ? int.MinValue : int.MaxValue;
+            List<Candidate> bestCandidates = new List<Candidate>();
             foreach (Move legalMove in noisyMoves)
             {
                 board.MakeMove(legalMove);
                 Candidate newCandidate = new Candidate(legalMove,0);
-                Candidate candidate = QuiesceneSearch(board, depth-1, !isMaximizing, newCandidate, alpha, beta);
-                if (bestCandidate.movement == Move.NullMove) bestCandidate=new Candidate(legalMove,candidate.materialWon);
+                Candidate candidate = QuiesceneSearch(board, depth-1, !isMaximizing, newCandidate, alpha, beta)[0];
+                if (bestCandidates.Count==0) bestCandidates.Add(new Candidate(legalMove,candidate.materialWon));
                 if (isMaximizing)
                 {
-                    if (candidate.materialWon>bestValue)
+                    if (candidate.materialWon>bestCandidates[0].materialWon)
                     {
-                        bestValue=candidate.materialWon;
-                        bestCandidate = new Candidate(legalMove, candidate.materialWon);
+                        bestCandidates.Insert(0,new Candidate(legalMove, candidate.materialWon));
+                    }
+                    else
+                    {
+                        bestCandidates.Add(new Candidate(legalMove, candidate.materialWon));
                     }
                     alpha = Math.Max(alpha, candidate.materialWon);
                 }
                 else
                 {
-                    if (candidate.materialWon<bestValue)
+                    if (candidate.materialWon<bestCandidates[0].materialWon)
                     {
-                        bestValue=candidate.materialWon;
-                        bestCandidate = new Candidate(legalMove, candidate.materialWon);
+                        bestCandidates.Insert(0,new Candidate(legalMove, candidate.materialWon));
+                    }
+                    else
+                    {
+                        bestCandidates.Add(new Candidate(legalMove, candidate.materialWon));
                     }
                     beta = Math.Min(beta,candidate.materialWon);
                 }
@@ -101,7 +110,7 @@ public class Test
                     break;
                 }
             }
-            return bestCandidate;
+            return bestCandidates;
         }
         else
         {
@@ -109,7 +118,9 @@ public class Test
             MaterialDifference(isWhite,board,depth)+
             PositionalDifference(isWhite,board,depth)+
             RateMovement(lastCandidate.movement,isWhite,board);
-            return lastCandidate;
+            List<Candidate> localList = new List<Candidate>();
+            localList.Add(lastCandidate);
+            return localList;
         }
     }
     public Test(bool isWhiteP)
